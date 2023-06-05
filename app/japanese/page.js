46b3e2f4e902625da1c77bf3476d32e1f39af2c7
shinjeoungho <hole546@naver.com>
2023-06-05 18:19:12 +0900

    Update: 디자인 수정 중

--- app/japanese/page.js
+++ app/japanese/page.js
@@ -6,12 +6,11 @@ const japanese = () => {
     { name: "메밀소바"},
     { name: "우동"},
     { name: "라멘"},
-    { name: "장어덮밥"},
+    { name: "덮밥"},
     { name: "야끼소바"},
     { name: "카레"},
     { name: "부타동"},
     { name: "초밥"},
-    { name: "후토마끼"},
   ];
   return (
     <>
--- app/korean/page.js
+++ app/korean/page.js
@@ -2,15 +2,13 @@ import Conmon from "../component/page";
 
 const korean = () => {
   const foodList = [
-    { name: "비빔밥"},
     { name: "부대찌개" },
     { name: "찜닭"},
     { name: "순대국"},
-    { name: "쌈밥" },
-    { name: "제육볶음" },
+    { name: "제육볶음"},
     { name: "된장찌개"},
-    { name: "갈비탕" },
-    { name: "김치찌개"},
+    { name: "갈비탕"},
+    { name: "설렁탕"},
     { name: "뼈 해장국"},
     { name: "감자탕" },
     { name: "라면" },
