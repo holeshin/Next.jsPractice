6d8abb21ce06441bd9da9f1acc3550b0ec6deaeb
shin <hole546@naver.com>
2023-07-03 13:15:14 +0900

    메뉴 추가

--- app/japanese/page.js
+++ app/japanese/page.js
@@ -3,14 +3,15 @@ import Conmon from "../component/page";
 const japanese = () => {
   const foodList = [
     { name: "돈까스" },
-    { name: "메밀소바"},
-    { name: "우동"},
-    { name: "라멘"},
-    { name: "덮밥"},
-    { name: "야끼소바"},
-    { name: "카레"},
-    { name: "부타동"},
-    { name: "초밥"},
+    { name: "메밀소바" },
+    { name: "우동" },
+    { name: "라멘" },
+    { name: "덮밥" },
+    { name: "야끼소바" },
+    { name: "카레" },
+    { name: "부타동" },
+    { name: "초밥" },
+    { name: "카레우동" },
   ];
   return (
     <>
